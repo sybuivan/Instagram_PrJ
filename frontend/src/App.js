8e201797aeadf850becebca129209f465471831a
sybuivan <sy.buivan@ncc.asia>
2022-07-08 00:23:04 +0700

    setup router app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRoutes } from 'react-router-dom';
+import { routes } from './constants';
+import './assets/css/reset.module.css';
+import './assets/css/_variable.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return useRoutes(routes);
 }
 
 export default App;
