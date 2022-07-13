f58d5f057ae655593148a4e7d1c885b9cb307a3e
sybuivan <sy.buivan@ncc.asia>
2022-07-13 23:21:03 +0700

    add @mui/styles, fix ui, add slider images

--- frontend/src/page/Login/Login.jsx
+++ frontend/src/page/Login/Login.jsx
@@ -1,16 +1,120 @@
-import React from 'react';
-import { Box, Container } from '@mui/material';
+import React, { useEffect, useRef, useState } from 'react';
+import { Box, Container, Typography } from '@mui/material';
 import LoginForm from './components/LoginForm';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import { resetTimeout } from '../../utils';
+
+const useStyles = makeStyles({
+  root: {
+    padding: '3rem 0',
+  },
+  boxImages: {
+    background:
+      'url(https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png)',
+    height: '58rem',
+    width: '100%',
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    '& img': {
+      width: '25rem',
+      position: 'absolute',
+      right: '7.4rem',
+      top: '2.6rem',
+      opacity: 0,
+    },
+  },
+  active: { opacity: '1!important', transition: 'ease 1000ms' },
+  boxTitle: {
+    border: '1px solid var(--border-gray)',
+    padding: '2.3rem 7.7rem',
+    '& span': { fontSize: '1.4rem' },
+    '& a': {
+      color: 'var(--color-blue)',
+      fontWeight: '600',
+    },
+  },
+  formBottom: {
+    '& span': {
+      fontSize: '1.6rem',
+      display: 'block',
+      padding: '2rem 0',
+    },
+    '& img': {
+      width: '13.6rem',
+      height: '4rem',
+      marginRight: '1rem',
+    },
+  },
+});
 const Login = () => {
+  const classes = useStyles();
+  const timeoutRef = useRef(null);
+  const [position, setPosition] = useState(0);
+  const slider = [
+    'https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png',
+    'https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png',
+    'https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png',
+  ];
+  useEffect(() => {
+    resetTimeout(timeoutRef);
+    timeoutRef.current = setTimeout(
+      () =>
+        setPosition((prevIndex) =>
+          prevIndex === slider.length - 1 ? 0 : prevIndex + 1
+        ),
+    3000
+    );
+    return () => {
+      resetTimeout(timeoutRef);
+    };
+  }, [position]);
+
   return (
-    <Box sx={{ padding: '3rem 0' }}>
-      <Container maxWidth="md" sx={{ display: 'flex', alignItems: 'center' }}>
-        <Box
-          component="img"
-          src="https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png"
-        />
+    <Box className={classes.root}>
+      <Container
+        maxWidth="md"
+        sx={{ display: 'flex', alignItems: 'center', overflow: 'hidden' }}
+      >
+        <Box className={classes.boxImages}>
+          {slider.map((slide, index) => (
+            <Box
+              component="img"
+              src={slide}
+              key={index}
+              className={position === index ? classes.active : ''}
+            />
+          ))}
+        </Box>
         <Box>
           <LoginForm />
+
+          <Box className={classes.boxTitle}>
+            <Typography variant="span">
+              Don't have an account? <Link to="/">Sign up</Link>
+            </Typography>
+          </Box>
+
+          <Box className={classes.formBottom}>
+            <Typography variant="span" align="center">
+              Get the app
+            </Typography>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Box
+                component="img"
+                src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png"
+              />
+              <Box
+                component="img"
+                src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png"
+              />
+            </Box>
+          </Box>
         </Box>
       </Container>
     </Box>
--- frontend/src/page/Login/components/LoginForm.jsx
+++ frontend/src/page/Login/components/LoginForm.jsx
@@ -1,30 +1,66 @@
 import React from 'react';
 import { Box, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import { AiFillFacebook } from 'react-icons/ai';
 import { images } from '../../../constants';
 import { InputField, PasswordField } from '../../../form-control';
-import { Link } from 'react-router-dom';
-import { FaFacebook } from 'react-icons/fa';
 
+const useStyles = makeStyles({
+  loginForm: {
+    width: '35rem',
+    border: '1px solid var(--border-gray)',
+    padding: '1.8rem 1rem',
+    marginBottom: '1rem',
+  },
+  logo: {
+    position: 'relative',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    padding: '2rem 0 ',
+  },
+  boxForm: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  line: {
+    backgroundColor: 'var(--border-gray)',
+    width: '45%',
+    height: '0.2rem',
+    position: 'relative',
+    top: '0.4rem',
+  },
+  boxButton: {
+    display: 'block',
+    width: '100%',
+    border: 'none',
+    padding: '2.3rem 0',
+    backgroundColor: 'transparent',
+    '& span': {
+      color: '#385185',
+      cursor: 'pointer',
+      '& svg': {
+        marginRight: '0.5rem',
+        fontSize: '1.7rem',
+        position: 'relative',
+        top: '0.4rem',
+      },
+    },
+  },
+  link: {
+    display: 'block',
+    textAlign: 'center',
+    fontSize: '1.5rem',
+    color: '#00376b',
+  },
+});
 const LoginForm = () => {
+  const classes = useStyles();
   return (
-    <Box
-      sx={{
-        width: '35rem',
-        border: '1px solid var(--border-gray)',
-        padding: '1.8rem 1rem',
-      }}
-    >
-      <Box
-        component="img"
-        src={images.LOGO}
-        sx={{
-          position: 'relative',
-          left: '50%',
-          transform: 'translateX(-50%)',
-        }}
-      />
+    <Box className={classes.loginForm}>
+      <Box component="img" src={images.LOGO} className={classes.logo} />
 
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Box className={classes.boxForm}>
         <Box sx={{ width: '25rem' }}>
           <InputField placeholder="Phone number, username, or email" />
           <PasswordField placeholder="Password" />
@@ -35,58 +71,19 @@ const LoginForm = () => {
           </Box>
 
           <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Box
-              sx={{
-                backgroundColor: 'var(--border-gray)',
-                width: '45%',
-                height: '0.2rem',
-                position: 'relative',
-                top: '0.4rem',
-              }}
-            ></Box>
+            <Box className={classes.line}></Box>
             <Box>OR</Box>
-            <Box
-              sx={{
-                backgroundColor: 'var(--border-gray)',
-                width: '45%',
-                height: '0.2rem',
-                position: 'relative',
-                top: '0.4rem',
-              }}
-            ></Box>
+            <Box className={classes.line}></Box>
           </Box>
 
           <Box>
-            <Box
-              component="button"
-              sx={{
-                display: 'block',
-                width: '100%',
-                border: 'none',
-                padding: '2.3rem 0',
-                backgroundColor: 'transparent',
-              }}
-            >
-              <Box
-                component="span"
-                sx={{
-                  '& svg': { marginRight: '0.5rem' },
-                  color: '#385185',
-                  cursor: 'pointer',
-                }}
-              >
-                <FaFacebook />
+            <Box component="button" className={classes.boxButton}>
+              <Box component="span">
+                <AiFillFacebook />
                 Login with facebook
               </Box>
             </Box>
-            <Link
-              to="/"
-              style={{
-                display: 'block',
-                textAlign: 'center',
-                fontSize: '1.5rem',
-              }}
-            >
+            <Link to="/" className={classes.link}>
               Forgot password?
             </Link>
           </Box>
