c5f145f88d61035c74aed8a13a510ca0a6becb6d
sybuivan <sy.buivan@ncc.asia>
2022-07-16 00:09:21 +0700

    add constans images slider

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,6 +1,10 @@
 import logo from '../assets/images/logo_login.png';
+import slider1 from '../assets/images/slider_1.png';
+import slider2 from '../assets/images/slider_2.png';
+import slider3 from '../assets/images/slider_3.png';
 const images = {
   LOGO: logo,
+  SLIDER: [slider1, slider2, slider3],
 };
 
 export default images;
