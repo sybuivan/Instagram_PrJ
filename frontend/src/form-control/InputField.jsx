241300df7963661057fe24ed322bda9e7ca5a353
sybuivan <sy.buivan@ncc.asia>
2022-07-16 00:12:33 +0700

    fix control-field when use react-hook-form

--- frontend/src/form-control/InputField.jsx
+++ frontend/src/form-control/InputField.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@mui/material';
+import { Controller } from 'react-hook-form';
 
-const Inputfield = ({ placeholder }) => {
+const Inputfield = ({ placeholder, control, name, errors }) => {
   return (
-    <TextField
-      id="outlined-basic"
-      variant="outlined"
-      placeholder={placeholder}
-      sx={{
-        width: '25rem',
-        height: '3.6rem!important',
-        marginBottom: '1rem',
-        '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
-          padding: '1rem 1.4rem',
-        },
-      }}
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <TextField
+          {...field}
+          id="outlined-basic"
+          variant="outlined"
+          placeholder={placeholder}
+          sx={{
+            width: '25rem',
+            height: '3.6rem!important',
+            marginBottom: !!errors[name] ? '2rem' : '1rem',
+            fontSize: '1rem',
+            '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
+              padding: '1rem 1.4rem',
+            },
+            '& .css-1wc848c-MuiFormHelperText-root': {
+              fontSize: '1rem',
+              marginLeft: 0,
+            },
+          }}
+          helperText={errors[name]?.message}
+          error={!!errors[name]}
+        />
+      )}
     />
   );
 };
--- frontend/src/form-control/PasswordField.jsx
+++ frontend/src/form-control/PasswordField.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@mui/material';
+import { Controller } from 'react-hook-form';
 
-const Passwordfield = ({ placeholder }) => {
+const Passwordfield = ({ placeholder, control, name, errors }) => {
   return (
-    <TextField
-      id="outlined-basic"
-      variant="outlined"
-      placeholder={placeholder}
-      type="password"
-      sx={{
-        width: '25rem',
-        marginBottom: '1.5rem',
-        height: '3.6rem!important',
-        '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
-          padding: '1rem 1.4rem',
-        },
-      }}
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <TextField
+          {...field}
+          id="outlined-basic"
+          variant="outlined"
+          placeholder={placeholder}
+          type="password"
+          sx={{
+            width: '25rem',
+            marginBottom: !!errors[name] ? '2rem' : '1rem',
+            height: '3.6rem!important',
+            '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
+              padding: '1rem 1.4rem',
+            },
+            '& .css-1wc848c-MuiFormHelperText-root': {
+              fontSize: '1rem',
+              marginLeft: 0,
+            },
+          }}
+          helperText={errors[name]?.message}
+          error={!!errors[name]}
+        />
+      )}
     />
   );
 };
