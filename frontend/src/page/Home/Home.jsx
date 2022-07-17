a15c3d03f729890b37ab80a698d99a6788e07822
sybuivan <sy.buivan@ncc.asia>
2022-07-18 01:43:05 +0700

    fix page home

--- frontend/src/page/Home/Home.jsx
+++ frontend/src/page/Home/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Outlet, useNavigate } from 'react-router';
+import { useSelector } from 'react-redux';
+import { Header } from '../../components';
 
 const Home = () => {
-  return <div>Home page</div>;
+  const navigate = useNavigate();
+  const isLogin = useSelector((state) => state.auth.current);
+  useEffect(() => {
+    if (!isLogin) {
+      navigate('/accounts/login');
+    }
+  }, []);
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 };
 
 Home.propTypes = {};
