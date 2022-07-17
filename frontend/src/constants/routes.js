b765a299b537f56938cbe8f9fc8634c2b3bdf99b
sybuivan <sy.buivan@ncc.asia>
2022-07-18 01:40:51 +0700

    fix config router

--- frontend/src/constants/routes.js
+++ frontend/src/constants/routes.js
@@ -1,15 +1,27 @@
-import { Home, Login, Register } from '../page';
-import { Auth } from '../page';
+import { Home, Login, Profile, Register, Saved, Message } from '../page';
+import { Auth } from '../page/Auth';
 let routes = [
   // A route object has the same properties as a <Route>
   // element. The `children` is just an array of child routes.
-  { path: '/', element: <Home /> },
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      { path: 'profile', element: <Profile /> },
+      { path: 'saved', element: <Saved /> },
+      {
+        path: 'message',
+        element: <Message />,
+      },
+    ],
+  },
   {
     path: 'accounts',
     element: <Auth />,
     children: [
       { path: 'login', element: <Login /> },
       { path: 'register', element: <Register /> },
+      { path: 'edit', element: <Register /> },
     ],
   },
 ];
