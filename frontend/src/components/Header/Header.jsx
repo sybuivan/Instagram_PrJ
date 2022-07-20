0d5101dbceee94d5109b54629135bb7a53d725af
sybuivan <sy.buivan@ncc.asia>
2022-07-20 17:52:31 +0700

    fix -show modal createPost in header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import { Grid, IconButton } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Box, Container } from '@mui/system';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { AiFillHome, AiOutlineHeart } from 'react-icons/ai';
 import { BiMessageSquareAdd } from 'react-icons/bi';
 import { FiSend } from 'react-icons/fi';
 import { RiAccountCircleFill } from 'react-icons/ri';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { BasicModal, CreateNewPost } from '..';
 import { images, navRoutes } from '../../constants';
+import { hiddenModal, showModal } from '../../page/Home/homeSlice';
 import Search from '../Search';
 import LinkItem from './LinkItem';
-import Menu from './Menu';
+import Menu from './MenuFrofile';
 import ResultRearch from './ResultRearch';
 
 const useStyles = makeStyles({
@@ -32,18 +35,57 @@ const useStyles = makeStyles({
     width: '10.3rem',
     height: '2.9rem',
   },
+  navIcon: {
+    position: 'relative',
+    '& > svg': {
+      fontSize: '2.5rem',
+    },
+  },
 });
 
 const Header = () => {
+  console.log('header');
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const isShowModal = useSelector((state) => state.home.modal);
+  const handleOnClickShowMore = () => {
+    dispatch(showModal('CREATE_POST'));
+  };
+  const handleOnClickHideModal = () => {
+    dispatch(hiddenModal('CREATE_POST'));
+  };
 
   const [openMenu, setOpenMenu] = useState(false);
   const [focused, setFocused] = useState(false);
   const [valueSearch, setValueSearch] = useState('');
+  const [listResult, setListResult] = useState([]);
+  const user = [
+    { userName: 'ha', fullName: 'ha nguyen' },
+    { userName: 'quang', fullName: 'ha nguyen' },
+    { userName: 'linh2', fullName: 'ha nguyen' },
+    { userName: 'halinh.23', fullName: 'ha nguyen' },
+  ];
   const handleOnChangeSearch = (value) => {
-    setFocused(true);
-    setValueSearch(value);
+    console.log(!value);
+    if (!!value) {
+      setFocused(true);
+      setValueSearch(value);
+      setListResult(user.filter((item) => item.userName.includes(value)));
+    } else {
+      setFocused(false);
+    }
   };
+
+  const memoizedCard = useMemo(() => {
+    return (
+      <BasicModal
+        component={<CreateNewPost />}
+        showModal={isShowModal.CREATE_POST}
+        onClickHideModal={handleOnClickHideModal}
+      />
+    );
+  }, [isShowModal]);
+
   return (
     <Box className={classes.root}>
       <Box className={classes.header}>
@@ -71,13 +113,23 @@ const Header = () => {
                   focused={focused}
                   valueSearch={valueSearch}
                 />
-                <ResultRearch focused={focused} />
+                <ResultRearch focused={focused} listResult={listResult} />
               </Box>
             </Grid>
 
             <Grid item md={4}>
               {navRoutes.map((item, index) => (
-                <LinkItem path={item?.path} icon={item.icon} key={index} />
+                <>
+                  {item?.path ? (
+                    <LinkItem path={item?.path} icon={item.icon} key={index} />
+                  ) : (
+                    <LinkItem
+                      icon={item.icon}
+                      key={index}
+                      onShowModal={handleOnClickShowMore}
+                    />
+                  )}
+                </>
               ))}
 
               <IconButton
@@ -95,6 +147,7 @@ const Header = () => {
             </Grid>
           </Grid>
         </Container>
+        {memoizedCard}
       </Box>
     </Box>
   );
