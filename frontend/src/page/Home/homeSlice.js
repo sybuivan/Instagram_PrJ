2c8c69e02341d823cb16edc611459cba790ba56d
sybuivan <sy.buivan@ncc.asia>
2022-07-20 17:51:28 +0700

    fix -state modal

--- frontend/src/page/Home/homeSlice.js
+++ frontend/src/page/Home/homeSlice.js
@@ -1,17 +1,49 @@
 import { createSlice } from '@reduxjs/toolkit';
 import storegeKeys from '../../constants/storegeKeys';
 const initialState = {
-  showModal: false,
+  modal: {
+    CREATE_POST: false,
+    UNFOLLOW: false,
+    MORE_POST: false,
+  },
 };
 export const homeSlice = createSlice({
   name: 'home',
   initialState,
   reducers: {
-    showModal: (state) => {
-      state.showModal = true;
+    showModal: (state, action) => {
+      switch (action.payload) {
+        case 'CREATE_POST':
+          state.modal.CREATE_POST = true;
+          break;
+        case 'UNFOLLOW': {
+          state.modal.UNFOLLOW = true;
+          break;
+        }
+        case 'MORE_POST': {
+          state.modal.MORE_POST = true;
+          break;
+        }
+        default:
+          break;
+      }
     },
-    hiddenModal: (state) => {
-      state.showModal = false;
+    hiddenModal: (state, action) => {
+      switch (action.payload) {
+        case 'CREATE_POST':
+          state.modal.CREATE_POST = false;
+          break;
+        case 'UNFOLLOW': {
+          state.modal.UNFOLLOW = false;
+          break;
+        }
+        case 'MORE_POST': {
+          state.modal.MORE_POST = false;
+          break;
+        }
+        default:
+          break;
+      }
     },
   },
 });
